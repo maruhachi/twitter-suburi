3585b35fe29e68096719a2a7f0b1fe7e076ab9d0
maruhachi <konchan1129@gmail.com>
2018-01-27 14:10:00 +0900

    first commit

---
+++ src/test/java/me/ykon/twittersuburi/TwitterSuburiApplicationTests.java
@@ -0,0 +1,16 @@
+package me.ykon.twittersuburi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitterSuburiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
